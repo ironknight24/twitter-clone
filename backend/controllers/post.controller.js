bc4aedcbca921cbf22a1702a947e0788bf633ab6
Deepak S <sdeepak.shankar@gmail.com>
2024-12-20 18:20:29 +0530

    get user posts

--- backend/controllers/post.controller.js
+++ backend/controllers/post.controller.js
@@ -211,10 +211,34 @@ export const getFollowingPosts = async (req, res) => {
     res.status(200).json(feedPosts);
   } catch (error) {
     console.log(error);
+    res.status(500).json({
+      message: "Something went wrong in getFollowingPosts Controller",
+    });
+  }
+};
+
+export const getUserPosts = async (req, res) => {
+  try {
+    const { username } = req.params;
+    const user = await User.findOne({ username });
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+    const posts = await Post.find({ user: user._id }).sort({ createdAt: -1 })
+      .populate({
+        path: "user",
+        select: "-password",
+      })
+      .populate({
+        path: "comments.user",
+        select: "-password",
+      });
+
+    res.status(200).json(posts);
+  } catch (error) {
+    console.log(error);
     res
       .status(500)
-      .json({
-        message: "Something went wrong in getFollowingPosts Controller",
-      });
+      .json({ message: "Something went wrong in getUserPosts Controller" });
   }
 };
--- backend/routes/post.routes.js
+++ backend/routes/post.routes.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import { protectRoute } from '../middleware/protectRoute.js';
-import { commentOnPost, createPost, deletePost, getAllPosts, getFollowingPosts, getLikedPosts, likeUnlikePost } from '../controllers/post.controller.js';
+import { commentOnPost, createPost, deletePost, getAllPosts, getFollowingPosts, getLikedPosts, getUserPosts, likeUnlikePost } from '../controllers/post.controller.js';
 
 const router = express.Router();
 
@@ -11,6 +11,7 @@ router.post('/create', protectRoute, createPost);
 router.delete('/:id', protectRoute, deletePost);
 router.get('/like/:id', protectRoute, likeUnlikePost);
 router.post('/comment/:id', protectRoute, commentOnPost);
+router.get('/user/:username', protectRoute, getUserPosts);
 
 
 export default router;
