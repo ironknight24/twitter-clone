aa82754528514e0666983347f98e2568fa0ca68f
Deepak S <sdeepak.shankar@gmail.com>
2024-12-20 18:02:04 +0530

    populate fields

--- backend/controllers/post.controller.js
+++ backend/controllers/post.controller.js
@@ -115,16 +115,16 @@ export const likeUnlikePost = async (req, res) => {
       await Post.updateOne({ _id: postId }, { $pull: { likes: userId } });
       res.status(200).json({ message: "Unliked post" });
     } else {
-        post.likes.push(userId);
-        await post.save();
+      post.likes.push(userId);
+      await post.save();
 
-        const notification = new Notification({
-            from: userId,
-            to: post.user,
-            type: "like",
-        })
-        await notification.save();
-        res.status(200).json({ message: "Liked post" });
+      const notification = new Notification({
+        from: userId,
+        to: post.user,
+        type: "like",
+      });
+      await notification.save();
+      res.status(200).json({ message: "Liked post" });
     }
   } catch (error) {
     console.log(error);
@@ -134,20 +134,27 @@ export const likeUnlikePost = async (req, res) => {
   }
 };
 
-
-export const getAllPosts = async(req, res) =>{
-    try {
-        const posts = await Post.find().sort({ createdAt: -1 });
-        if (posts.length === 0) {
-            return res.status(200).json([])
-        }
-
-        res.status(200).json(posts);
-        
-    } catch (error) {
-        console.log(error);
-        res
-        .status(500)
-        .json({ message: "Something went wrong in getAllPosts Controller" });
+export const getAllPosts = async (req, res) => {
+  try {
+    const posts = await Post.find()
+      .sort({ createdAt: -1 })
+      .populate({
+        path: "user",
+        select: "-password",
+      })
+      .populate({
+        path: "comments.user",
+        select: "-password",
+      });
+    if (posts.length === 0) {
+      return res.status(200).json([]);
     }
-}
+
+    res.status(200).json(posts);
+  } catch (error) {
+    console.log(error);
+    res
+      .status(500)
+      .json({ message: "Something went wrong in getAllPosts Controller" });
+  }
+};
